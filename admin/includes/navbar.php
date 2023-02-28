277c6737db8bfa653d2039847ec25be597072124
Bayou76 <103516275+Bayou76@users.noreply.github.com>
2023-02-28 14:16:35 +0100

    Update navbar.php

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -10,6 +10,7 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <?php 
             if(isset($_SESSION['username'])){
+              
               ?>
               <li class="nav-item">
                 <a class="nav-link" href="../../admin/views/inscriptionEmploye.php">Inscrire un nouveau employé</a>
